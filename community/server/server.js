888484d41b1b3761132a8b261c98d8aa1065ac2f
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-07-09 20:58:56 +0900

    mongodb 연결

--- community/server/server.js
+++ community/server/server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3001;
-
+const mongoose = require('mongoose');
+const db = require('./config/keys');
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.listen(port, () => {
-  console.log('listening on port 3001');
+  mongoose
+    .connect(db.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(`${err}`));
 });
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
