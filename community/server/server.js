073099ae0f68df4df160a0d822a7ad823677ff45
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-07-09 18:59:55 +0900

    build & express server

--- community/server/server.js
+++ community/server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3001;
 
-app.listen(3001, () => {
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.listen(port, () => {
   console.log('listening on port 3001');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
